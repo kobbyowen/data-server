0c18d667aa622b1adb7a47f6a49fee29c9521c9a
Kobby Owen <okessuman@st.ug.edu.gh>
2022-11-25 09:58:21 +0000

    Added comments

--- data_server/errors.py
+++ data_server/errors.py
@@ -1,6 +1,18 @@
-class ItemNotFoundError(Exception):
+class DataServerError(Exception):
+    def __init__(self, code: int, description: str):
+        super().__init__()
+        self.code = code
+        self.description = description
+
+
+class DataControllerError(DataServerError):
+    def __init__(self, description: str):
+        super().__init__(400, description)
+
+
+class ItemNotFoundError(DataControllerError):
     pass
 
 
-class DuplicateIDFound(Exception):
+class DuplicateIDFound(DataControllerError):
     pass
