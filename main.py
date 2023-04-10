eb7ab5eca15659fe951f94ba0233f475363d4d7a
Kobby Owen <okessuman@st.ug.edu.gh>
2023-04-10 10:47:41 +0000

    Added more tests

--- main.py
+++ main.py
@@ -3,33 +3,33 @@ from data_server.core.server import Server
 from data_server.core.data_router import DataRouter
 
 
-parser = ArgumentParser(
-    "Data Server",
-    "Spin up a full fake REST API with no coding in less than 3 \
-                            seconds using JSON , CSV file as the source.")
+def main() -> None:
+    parser = ArgumentParser(
+        "Data Server",
+        "Spin up a full fake REST API with no coding in less than 3 \
+                                seconds using JSON , CSV file as the source.")
 
-arguments = parser.get_parsed_arguments()
+    arguments = parser.get_parsed_arguments()
 
-request_handler = DataRouter(
-    arguments["file"],
-    id_name=arguments["id_name"],
-    sort_key_param_name=arguments["sort_param_name"],
-    order_param_name=arguments["order_param_name"],
-    page_param_name=arguments["page_param_name"],
-    size_param_name=arguments["size_param_name"],
-    autogenerate_id=arguments["auto_generate_ids"],
-    use_timestamps=arguments["use_timestamps"],
-    created_at_key_name=arguments["created_at_key_name"],
-    updated_at_key_name=arguments["updated_at_key_name"],
-    default_page_size=arguments["page_size"])
+    request_handler = DataRouter(
+        arguments["file"],
+        id_name=arguments["id_name"],
+        sort_key_param_name=arguments["sort_param_name"],
+        order_param_name=arguments["order_param_name"],
+        page_param_name=arguments["page_param_name"],
+        size_param_name=arguments["size_param_name"],
+        autogenerate_id=arguments["auto_generate_ids"],
+        use_timestamps=arguments["use_timestamps"],
+        created_at_key_name=arguments["created_at_key_name"],
+        updated_at_key_name=arguments["updated_at_key_name"],
+        default_page_size=arguments["page_size"])
 
-server = Server(request_handler=request_handler,
-                url_path_prefix=arguments["url_path_prefix"],
-                host=arguments["host"],
-                port=arguments["port"],
-                static_url_prefix=arguments["static_url_prefix"],
-                additional_headers=arguments["additional_headers"],
-                sleep_before_request=arguments["sleep_before_request"],
-                extra_files=[arguments["file"]])
-
-server.run()
+    server = Server(request_handler=request_handler,
+                    url_path_prefix=arguments["url_path_prefix"],
+                    host=arguments["host"],
+                    port=arguments["port"],
+                    static_url_prefix=arguments["static_url_prefix"],
+                    additional_headers=arguments["additional_headers"],
+                    sleep_before_request=arguments["sleep_before_request"],
+                    extra_files=[arguments["file"]])
+    server.run()
---
+++ tests/unit/test_main.py
@@ -0,0 +1,40 @@
+import unittest
+from unittest.mock import patch
+from main import main
+
+
+class TestMain(unittest.TestCase):
+    def setUp(self) -> None:
+        argument_parser_patch = patch("main.ArgumentParser")
+        server_patch = patch("main.Server")
+        data_router_patch = patch("main.DataRouter")
+        self.argument_parser_mock = argument_parser_patch.start()
+        self.server_mock = server_patch.start()
+        self.data_router_mock = data_router_patch.start()
+        self.argument_parser_instance = self.argument_parser_mock.return_value
+        self.argument_parser_instance.get_parsed_arguments.return_value = {
+            "file": "", "id_name": "id", "sort_param_name": "sort",
+            "order_param_name": "order", "page_param_name": "page",
+            "size_param_name": "size", "auto_generate_ids": True,
+            "use_timestamps": True, "created_at_key_name": "created_at",
+            "updated_at_key_name": "updated_at", "page_size": 10,
+            "url_path_prefix": "/", "host": "localhost", "port": 2020,
+            "static_url_prefix": "static", "additional_headers": "",
+            "sleep_before_request": 10, }
+        self.server_instance = self.server_mock.return_value
+        self.server_instance.run.return_value = None
+        self.data_router_instance = self.data_router_mock.return_value
+        super().setUp()
+
+    def tearDown(self) -> None:
+        self.argument_parser_mock.stop()
+        self.server_mock.stop()
+        self.data_router_mock.stop()
+        super().tearDown()
+
+    def test_main(self) -> None:
+        main()
+        self.assertTrue(
+            self.argument_parser_instance.get_parsed_arguments.called)
+        self.assertTrue(
+            self.server_instance.run.called)
